2ac6436cf8fe9374ac5f6f974321f3c195a29f2f
Lexeor <lexeor.ross@gmail.com>
2023-06-27 18:28:26 +0400

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -37,16 +37,3 @@ class Queue {
     console.log(result);
   }
 }
-
-let queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-queue.enqueue(4);
-queue.enqueue(1);
-queue.print();
-queue.dequeue();
-queue.dequeue();
-queue.print();
-console.log(queue.peek());
