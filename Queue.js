f9e691d08aece3c221072abae83cc504c412ddd9
Lexeor <lexeor.ross@gmail.com>
2023-06-27 15:29:11 +0400

    Queue class optimized

--- Queue.js
+++ Queue.js
@@ -1,32 +1,52 @@
 class Queue {
   constructor() {
-    this.list = [];
+    this.list = {};
+    this.front = 0;
+    this.rear = 0;
   }
 
   enqueue(element) {
-    this.list.push(element);
+    this.list[this.rear] = element;
+    this.rear++;
   }
 
   dequeue() {
-    return this.list.shift();
+    const item = this.list[this.front];
+    delete this.list[this.front];
+    this.front++;
+    return item;
   }
 
   size() {
-    return this.list.length;
+    return this.rear - this.front;
   }
 
   peek() {
-    if (!this.isEmpty()) {
-      return this.list[0];
-    }
-    return null;
+    return this.list[this.front];
   }
 
   isEmpty() {
-    return this.list.length === 0;
+    return this.rear - this.front === 0;
   }
 
   print() {
-    console.log(this.list.toString());
+    let result = "";
+    for (let item in this.list) {
+      result += `${this.list[item].toString()} `;
+    }
+    console.log(result);
   }
 }
+
+let queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+queue.enqueue(1);
+queue.print();
+queue.dequeue();
+queue.dequeue();
+queue.print();
+console.log(queue.peek());
