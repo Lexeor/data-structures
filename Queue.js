a4f5d551481d39998cebc3a2c0367ad02a38f8dd
Lexeor <lexeor.ross@gmail.com>
2023-06-29 17:40:50 +0400

    Queue JSDoc added

--- Queue.js
+++ Queue.js
@@ -1,3 +1,4 @@
+/** First In - First Out (FIFO) */
 class Queue {
   constructor() {
     this.list = {};
@@ -5,13 +6,17 @@ class Queue {
     this.rear = 0;
   }
 
-  /** Add element to the end of the queue */
+  /** Add element to the end of the queue.
+   * @param {number} [element] element to be added.
+   */
   enqueue(element) {
     this.list[this.rear] = element;
     this.rear++;
   }
 
-  /** Remove element from the start of the queue */
+  /** Remove element from the start of the queue.
+   * @returns {number} removed element.
+   */
   dequeue() {
     const item = this.list[this.front];
     delete this.list[this.front];
@@ -19,18 +24,28 @@ class Queue {
     return item;
   }
 
+  /** Returns current size of the queue.
+   * @returns {number} queue size.
+   */
   size() {
     return this.rear - this.front;
   }
 
+  /** Returns the first element of the queue.
+   *  @returns {number} first queue element.
+   */
   peek() {
     return this.list[this.front];
   }
 
+  /** Returns is queue empty or not.
+   * @returns {boolean} queue is empty.
+   */
   isEmpty() {
     return this.rear - this.front === 0;
   }
 
+  /** Prints all queue to the console as object. */
   print() {
     let result = "";
     for (let item in this.list) {
