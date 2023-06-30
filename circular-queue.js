d16b4ca0b185f8d600537f8394d16eed00a90c26
Lexeor <lexeor.ross@gmail.com>
2023-06-30 19:02:28 +0400

    enqueue and print methods implemented

--- circular-queue.js
+++ circular-queue.js
@@ -1,4 +1,4 @@
-export class CircularQueue {
+class CircularQueue {
   constructor(capacity) {
     this.list = {};
     this.capacity = capacity;
@@ -7,7 +7,18 @@ export class CircularQueue {
     this.rear = -1;
   }
 
-  enqueue(element) {}
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.list[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
+    } else {
+      console.log("Queue is full.");
+    }
+  }
 
   dequeue() {}
 
@@ -23,8 +34,24 @@ export class CircularQueue {
 
   size() {}
 
-  print() {}
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty.");
+    } else {
+      let i;
+      let result = "";
+      for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+        result += `${this.list[i]}, `;
+      }
+      result += this.list[i];
+      console.log(result);
+    }
+  }
 }
 
-let cl = new CircularQueue();
+let cl = new CircularQueue(3);
 cl.enqueue(10);
+cl.enqueue(10);
+cl.enqueue(10);
+cl.enqueue(10);
+cl.print();
