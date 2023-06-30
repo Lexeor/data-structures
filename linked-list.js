9ebfb143b8b0da053c6dbe3a84c00d4ed43f3732
Lexeor <lexeor.ross@gmail.com>
2023-06-30 15:34:58 +0400

    print method implemented

--- linked-list.js
+++ linked-list.js
@@ -31,9 +31,24 @@ class LinkedList {
     }
     this.size++;
   }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty.");
+    } else {
+      let result = [];
+      let curr = this.head;
+      while (curr) {
+        result.push(curr.value);
+        curr = curr.next;
+      }
+      console.log(result.join(", "));
+    }
+  }
 }
 
 const list = new LinkedList();
 list.prepend(5);
 list.prepend(20);
 list.prepend(70);
+list.print();
